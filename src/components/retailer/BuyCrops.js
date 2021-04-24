ca70e1db76788bf30eea6ab160d8df7f3a71a956
Anjel-Patel <anjelpatel54@gmail.com>
2021-04-24 16:38:48 +0530

    Added search bar

--- src/components/retailer/BuyCrops.js
+++ src/components/retailer/BuyCrops.js
@@ -50,8 +50,29 @@ function BuyCrops() {
                     </div>
                 </div>
             </div>
+            
             <div className="main-area">
-                {CropMaker(crops)}
+                <div style={{display:'flex'}}>
+                    <div style={{flex:1}}></div>
+                    <div className="search-bar" style={{display:'flex', marginTop:'32px', alignItems:'center'}}>
+                        <input className="search-bar-field" type="text" placeholder="Search"/>
+                        <div onClick={""}>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0)">
+                            <path d="M23.7068 22.293L16.8818 15.4681C18.2038 13.8351 18.9998 11.7601 18.9998 9.50018C18.9998 4.26222 14.7378 0.000244141 9.49988 0.000244141C4.26193 0.000244141 0 4.26218 0 9.50013C0 14.7381 4.26197 19.0001 9.49992 19.0001C11.7599 19.0001 13.8349 18.2041 15.4678 16.8821L22.2928 23.707C22.4878 23.902 22.7438 24 22.9998 24C23.2558 24 23.5118 23.902 23.7068 23.707C24.0978 23.316 24.0978 22.684 23.7068 22.293ZM9.49992 17.0001C5.36395 17.0001 2 13.6361 2 9.50013C2 5.36415 5.36395 2.0002 9.49992 2.0002C13.6359 2.0002 16.9998 5.36415 16.9998 9.50013C16.9998 13.6361 13.6359 17.0001 9.49992 17.0001Z" fill="#666666"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0">
+                            <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                            </defs>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="crops-area">
+                    {CropMaker(crops)}
+                </div>
             </div>
         </div>
     );
